cd677defef055296751a23dc4dda7945f661c40e
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-06-14 13:19:53 -0700

    Update myCred.js

--- myCred.js
+++ myCred.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({  // create connection pool
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu', // server address
   user: 'cs290_parkhyey',
-  password: 'java4844',
+  password: 'xxxxxx',
   database: 'cs290_parkhyey'
 });
 
